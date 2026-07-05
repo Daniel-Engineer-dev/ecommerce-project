ffcf44e548396489049dee025929c25d3efd1c61
Daniel-Engineer-dev <le2487009@gmail.com>
2026-07-05 17:30:54 +0700

    chore: stop tracking local working docs and submission package
    Remove Requirement/, SUBMISSION_PACKAGE/, and content_management_implementation_plan.md from version control and add them to .gitignore. Files remain locally.

--- SUBMISSION_PACKAGE/02_source_code/source_code/application/server/modules/shared/contentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../admin/adminController');
-
-router.get('/public', adminController.getPublicContentItems);
-router.get('/public/:slug', adminController.getPublicContentBySlug);
-
-module.exports = router;
